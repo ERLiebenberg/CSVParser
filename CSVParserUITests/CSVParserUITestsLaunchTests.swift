356261c1001ae346a7b4b339e7085375d249e2f6
ERLiebenberg <>
2022-06-11 11:06:58 +0200

    add empty project called CSVParser
    - add .gitignore file ignoring xcuserdata

---
+++ CSVParserUITests/CSVParserUITestsLaunchTests.swift
@@ -0,0 +1,29 @@
+//
+//  CSVParserUITestsLaunchTests.swift
+//  CSVParserUITests
+//
+//  Created by Errol Liebenberg on 2022/06/11.
+//
+
+import XCTest
+
+class CSVParserUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
