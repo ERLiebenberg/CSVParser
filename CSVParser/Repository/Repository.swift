bdc8cb653dc7493500f0a29f4f8f217d25b8649a
ERLiebenberg <>
2022-06-12 20:02:06 +0200

    update repository callback to use weak self

--- CSVParser/Repository/Repository.swift
+++ CSVParser/Repository/Repository.swift
@@ -43,14 +43,14 @@ class RepositoryImplementation {
 extension RepositoryImplementation: Repository {
     
     func loadItems(url: URL, result: @escaping ([Item], Error?) -> ()) {
-        parser.parse(path: url.path, result: { records, error in
+        parser.parse(path: url.path, result: { [weak self] records, error in
             guard let records = records, error == nil else {
                 result([], error)
                 return
             }
         
             do {
-                let items = try self.mapItems(from: records)
+                let items = try self?.mapItems(from: records) ?? []
                 result(items, nil)
             } catch {
                 result([], error)
